faee921c70e7bf8bc727471764e9e2f87f6a7fab
Ludvig Böklin <ludvig.boklin@proton.me>
2024-04-29 14:28:05 +0200

    small note

--- comfyui/default.nix
+++ comfyui/default.nix
@@ -3,6 +3,8 @@
     cfg.comfyui = system: let
       pkgs = import inputs.nixpkgs { inherit system; };
       models = pkgs.callPackage ./models {};
+      # this can be slightly confusing because this is *not* the same as base_path in extra_model_paths.yaml;
+      # this is the base directory wherein misc mutable data is stored
       basePath = "/var/lib/comfyui";
     in {
       inherit models;
